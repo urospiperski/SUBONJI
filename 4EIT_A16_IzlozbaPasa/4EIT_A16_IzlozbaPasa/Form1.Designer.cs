89ea43439d0aa81ea271276df684366d5a953c77
urospiperski <urkepiperski@gmail.com>
2025-06-11 17:30:08 +0200

    Add files via upload

---
+++ 4EIT_A16_IzlozbaPasa/4EIT_A16_IzlozbaPasa/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace _4EIT_A16_IzlozbaPasa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(759, 426);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.comboBox3);
+            this.tabPage1.Controls.Add(this.comboBox2);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(751, 397);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(421, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(289, 175);
+            this.pictureBox1.TabIndex = 26;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(247, 273);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 71);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "zatvori";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 71);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "prijava";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(104, 177);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(229, 24);
+            this.comboBox3.TabIndex = 23;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(104, 111);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(229, 24);
+            this.comboBox2.TabIndex = 22;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(104, 53);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(229, 24);
+            this.comboBox1.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "kategorija";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "izlozba";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "pas ";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.chart1);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Controls.Add(this.comboBox4);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(751, 397);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(206, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "ukupan broj pasa koji se takmicio";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(206, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "ukupan broj pasa koji je prijavljen";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(406, 29);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(325, 339);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(178, 342);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 37);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(39, 342);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 37);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 127);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(341, 199);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(91, 23);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(240, 24);
+            this.comboBox4.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "izlozba";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(751, 397);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(751, 397);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 452);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 4EIT_A16_IzlozbaPasa/4EIT_A16_IzlozbaPasa/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace _4EIT_A16_IzlozbaPasa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection Kon=new SqlConnection(@"Data Source=URKE\SQLEXPRESS;Initial Catalog=4EIT_A16_IzlozbaPasa;Integrated Security=True");
+        SqlCommand kom=new SqlCommand();
+        SqlDataReader dr;
+        int id = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tabPage1.Text = "Unesi prijavu";
+            tabPage2.Text = "Statistika";
+            tabPage3.Text = "O aplikaciji";
+            tabPage4.Text = "Izlaz";
+            punicomboPas();
+            punicomboIzlozba();
+            punicomboKategorija();
+          
+        }
+        private void punicomboPas()
+        {
+            Kon.Open();
+            SqlCommand cmd = new SqlCommand("PuniComboPas", Kon);
+            cmd.CommandType = CommandType.StoredProcedure;
+          
+            DataTable dt = new DataTable();
+            dt.Load(cmd.ExecuteReader());
+            comboBox1.DataSource = dt;
+            comboBox1.DisplayMember = "Pas";
+            Kon.Close();
+        }
+
+        private void punicomboIzlozba()
+        {
+            Kon.Open();
+            SqlCommand cmd = new SqlCommand("PuniComboIzlozba", Kon);
+            cmd.CommandType = CommandType.StoredProcedure;
+          
+            DataTable dt = new DataTable();
+            dt.Load(cmd.ExecuteReader());
+            comboBox2.DataSource = dt;
+            comboBox2.DisplayMember = "Izlozba";
+            Kon.Close();
+        }
+
+        private void punicomboKategorija()
+        {
+            Kon.Open();
+            SqlCommand cmd = new SqlCommand("PuniComboKategorija", Kon);
+            cmd.CommandType = CommandType.StoredProcedure;
+            
+            DataTable dt = new DataTable();
+            dt.Load(cmd.ExecuteReader());
+            comboBox3.DataSource = dt;
+            comboBox3.DisplayMember = "Kategorija";
+            Kon.Close();
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PrijaviPsa();
+        }
+
+        private void Provera()
+        {
+            Kon.Open();
+
+            string Pas = comboBox1.Text.ToString();
+            string[] PasID = Pas.Split('-');
+            string Izlozba = comboBox2.Text.ToString();
+            string[] IzlozbaID = Izlozba.Split('-');
+            string Kategorija = comboBox3.Text.ToString();
+            string[] KategorijaID = Kategorija.Split('-');
+
+            SqlCommand cmd = new SqlCommand("Provera", Kon);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@IzlozbaID", SqlDbType.NVarChar).Value = IzlozbaID[0];
+            cmd.Parameters.AddWithValue("@KategorijaID", SqlDbType.Int).Value = Convert.ToInt32(KategorijaID[0]);
+            cmd.Parameters.AddWithValue("@PasID", SqlDbType.Int).Value = Convert.ToInt32(PasID[0]);
+
+            string Pass = "";
+            string Izlozbaa = "";
+            string Kategorijaa = "";
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Pass = dr[2].ToString().Trim();
+                Izlozbaa = dr[0].ToString().Trim();
+                Kategorijaa = dr[1].ToString().Trim();
+            }
+
+            Kon.Close();
+
+            if (PasID[0].Trim() == Pass && IzlozbaID[0].Trim() == Izlozbaa && KategorijaID[0].Trim() == Kategorijaa)
+            {
+                MessageBox.Show("Vec ste se prijavili", "Greska");
+            }
+            else
+            {
+                PrijaviPsa();
+            }
+        }
+
+        private void PrijaviPsa()
+        {
+            Kon.Open();
+
+            string Pas = comboBox1.Text.ToString();
+            string[] PasID = Pas.Split('-');
+            string Izlozba = comboBox2.Text.ToString();
+            string[] IzlozbaID = Izlozba.Split('-');
+            string Kategorija = comboBox3.Text.ToString();
+            string[] KategorijaID = Kategorija.Split('-');
+
+            SqlCommand cmd = new SqlCommand("PrijavaPasa", Kon);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@IzlozbaID", SqlDbType.NVarChar).Value = IzlozbaID[0];
+            cmd.Parameters.AddWithValue("@KategorijaID", SqlDbType.Int).Value = Convert.ToInt32(KategorijaID[0]);
+            cmd.Parameters.AddWithValue("@PasID", SqlDbType.Int).Value = Convert.ToInt32(PasID[0]);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Uspesno ste se prijavili!");
+            Kon.Close();
+        }
+        private void PuniComboIzlozba2()
+        {
+            Kon.Open();
+
+            SqlCommand cmd = new SqlCommand("PuniComboIzlozba", Kon);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            DataTable dt = new DataTable();
+            dt.Load(cmd.ExecuteReader());
+
+            comboBox4.DataSource = dt;
+            comboBox4.DisplayMember = "Izlozba";
+
+            Kon.Close();
+        }
+        private void PuniGridIChart()
+        {
+            chart1.Titles.Clear();
+            string Izlozba = comboBox4.Text.ToString();
+            string[] IzlozbaID = Izlozba.Split('-');
+            Kon.Open();
+
+            SqlCommand cmd = new SqlCommand("PuniGridIChart", Kon);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@IzlozbaID", SqlDbType.NVarChar).Value = IzlozbaID[0];
+
+            DataTable dt = new DataTable();
+            dt.Load(cmd.ExecuteReader());
+
+            dataGridView1.DataSource = dt;
+            chart1.DataSource = dt;
+
+            chart1.Series["Series1"].XValueMember = "Naziv";
+            chart1.Series["Series1"].YValueMembers = "BrojPasa";
+            chart1.Titles.Add("Broj pasa koji se takmicio");
+
+            Kon.Close();
+        }
+
+        private void UkupanPrijavljen()
+        {
+            Kon.Open();
+
+            string Izlozba = comboBox4.Text.ToString();
+            string[] IzlozbaID = Izlozba.Split('-');
+
+            SqlCommand cmd = new SqlCommand("UkupanBrPrijavljenih", Kon);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@IzlozbaID", SqlDbType.NVarChar).Value = IzlozbaID[0];
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                label5.Text = "Ukupan broj pasa koji je prijavljen : " + dr[0].ToString();
+            }
+
+            Kon.Close();
+        }
+
+        private void UkupanTakmicen()
+        {
+            Kon.Open();
+
+            string Izlozba = comboBox4.Text.ToString();
+            string[] IzlozbaID = Izlozba.Split('-');
+
+            SqlCommand cmd = new SqlCommand("UkupanBrKojiSeTakmicio", Kon);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@IzlozbaID", SqlDbType.NVarChar).Value = IzlozbaID[0];
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                label6.Text = "Ukupan broj pasa koji se takmicio : " + dr[0].ToString();
+            }
+
+            Kon.Close();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PuniGridIChart();
+            UkupanPrijavljen();
+            UkupanTakmicen();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+            PuniComboIzlozba2();
+        }
+    }
+}
